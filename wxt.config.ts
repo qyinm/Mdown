560596c16bcb8ed6f2bab06edb4fd8c6cc64a578
qyinm <qusseun@gmail.com>
2026-01-31 13:50:32 +0900

    Configure extension manifest with permissions

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'wxt';
 
-// See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-vue'],
+  manifest: {
+    name: 'Mdown',
+    description: 'Save web pages as clean Markdown files',
+    permissions: ['activeTab', 'downloads'],
+  },
 });
