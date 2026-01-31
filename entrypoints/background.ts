504dcbe4793bf71c361594cb4bc06f574c44de31
qyinm <qusseun@gmail.com>
2026-01-31 13:42:56 +0900

    Add download functionality in background script

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,3 +1,31 @@
 export default defineBackground(() => {
-  console.log('Hello background!', { id: browser.runtime.id });
+  browser.runtime.onMessage.addListener((message, _sender, sendResponse) => {
+    if (message.action === 'download') {
+      downloadMarkdown(message.title, message.markdown)
+        .then(() => sendResponse({ success: true }))
+        .catch((e) => sendResponse({ error: e.message }));
+      return true;
+    }
+  });
 });
+
+async function downloadMarkdown(title: string, markdown: string): Promise<void> {
+  const filename = sanitizeFilename(title) + '.md';
+  const blob = new Blob([markdown], { type: 'text/markdown' });
+  const url = URL.createObjectURL(blob);
+
+  await browser.downloads.download({
+    url,
+    filename,
+    saveAs: true,
+  });
+
+  URL.revokeObjectURL(url);
+}
+
+function sanitizeFilename(name: string): string {
+  return name
+    .replace(/[<>:"/\\|?*]/g, '')
+    .replace(/\s+/g, '_')
+    .substring(0, 100);
+}
